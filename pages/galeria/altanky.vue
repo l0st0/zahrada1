6af6d6702039ae273c05f09a8cc9f99bb736ac18
l0st0 <r.strateny@gmail.com>
2019-12-12 17:07:56 +0100

    lazyload

--- pages/galeria/altanky.vue
+++ pages/galeria/altanky.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <Header header="Altánky" />
-    <v-gallery :images="list"></v-gallery>
+    <v-gallery :images="list">
+      <a v-for="img in list" :data-image="img.url">
+        <div class="image-container">
+          <img class="lazyload" :data-src="img.url" />
+        </div>
+      </a>
+    </v-gallery>
   </div>
 </template>
 
@@ -14,40 +20,40 @@ export default {
   data() {
     return {
       list: [
-        { title: "Image2", url: require("~/assets/img/altany/1.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/2.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/3.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/4.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/5.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/6.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/7.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/8.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/9.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/10.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/11.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/12.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/13.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/14.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/15.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/16.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/17.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/18.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/19.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/20.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/21.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/22.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/23.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/24.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/25.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/26.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/27.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/28.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/29.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/30.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/31.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/32.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/33.jpg") },
-        { title: "Image2", url: require("~/assets/img/altany/34.jpg") }
+        { url: require("~/assets/img/altany/1.jpg") },
+        { url: require("~/assets/img/altany/2.jpg") },
+        { url: require("~/assets/img/altany/3.jpg") },
+        { url: require("~/assets/img/altany/4.jpg") },
+        { url: require("~/assets/img/altany/5.jpg") },
+        { url: require("~/assets/img/altany/6.jpg") },
+        { url: require("~/assets/img/altany/7.jpg") },
+        { url: require("~/assets/img/altany/8.jpg") },
+        { url: require("~/assets/img/altany/9.jpg") },
+        { url: require("~/assets/img/altany/10.jpg") },
+        { url: require("~/assets/img/altany/11.jpg") },
+        { url: require("~/assets/img/altany/12.jpg") },
+        { url: require("~/assets/img/altany/13.jpg") },
+        { url: require("~/assets/img/altany/14.jpg") },
+        { url: require("~/assets/img/altany/15.jpg") },
+        { url: require("~/assets/img/altany/16.jpg") },
+        { url: require("~/assets/img/altany/17.jpg") },
+        { url: require("~/assets/img/altany/18.jpg") },
+        { url: require("~/assets/img/altany/19.jpg") },
+        { url: require("~/assets/img/altany/20.jpg") },
+        { url: require("~/assets/img/altany/21.jpg") },
+        { url: require("~/assets/img/altany/22.jpg") },
+        { url: require("~/assets/img/altany/23.jpg") },
+        { url: require("~/assets/img/altany/24.jpg") },
+        { url: require("~/assets/img/altany/25.jpg") },
+        { url: require("~/assets/img/altany/26.jpg") },
+        { url: require("~/assets/img/altany/27.jpg") },
+        { url: require("~/assets/img/altany/28.jpg") },
+        { url: require("~/assets/img/altany/29.jpg") },
+        { url: require("~/assets/img/altany/30.jpg") },
+        { url: require("~/assets/img/altany/31.jpg") },
+        { url: require("~/assets/img/altany/32.jpg") },
+        { url: require("~/assets/img/altany/33.jpg") },
+        { url: require("~/assets/img/altany/34.jpg") }
       ]
     };
   }
@@ -55,7 +61,7 @@ export default {
 </script>
 
 <style lang="scss">
-.light-gallery {
+.lightBoxGallery {
   display: flex !important;
   align-items: center;
   justify-content: center;
@@ -74,6 +80,7 @@ export default {
     height: auto !important;
 
     img {
+      cursor: pointer;
       width: 16rem !important;
       height: 15rem !important;
       box-shadow: 0 0 6px 2px black;
